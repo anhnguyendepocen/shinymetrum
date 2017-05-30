fefd670426288403b6e917cf49d5cd920b3518fc
Eric Anderson <andersone@Erics-MacBook-Pro.local>
2017-05-30 11:34:06 -0400

    dont need bootstrapLib

--- R/metworx-app.R
+++ R/metworx-app.R
@@ -14,7 +14,6 @@ metworxApp <- function(..., metworxTitle = "Metworx Shiny App"){
         rel = "shortcut icon",
         href = "https://metrumrg-soft.s3.amazonaws.com/shinyapps/shinymetrum/favicon.ico"
       ),
-      shiny::bootstrapLib(),
       shiny::includeCSS(
         system.file(
           "shiny-metworx.css",
