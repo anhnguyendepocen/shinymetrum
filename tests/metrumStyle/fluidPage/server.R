03e95aa0c162b083d0a153552b971b7db9f6286e
Eric Anderson <andersone@Erics-MacBook-Pro.local>
2017-06-01 10:31:28 -0400

    updated test

---
+++ tests/metrumStyle/fluidPage/server.R
@@ -0,0 +1,10 @@
+function(input, output) {
+  output$phonePlot <- shiny::renderPlot({
+    barplot(
+      datasets::WorldPhones[,input$region]*1000, 
+      main=input$region,
+      ylab="Number of Telephones",
+      xlab="Year"
+    )
+  })
+}
---
+++ tests/metrumStyle/fluidPage/ui.R
@@ -0,0 +1,20 @@
+shiny::fluidPage(
+  # metrumStyle(),
+  shiny::tags$h2("Telephones by region"),
+  shiny::fluidRow(
+    shiny::column(
+      width = 4,
+      shiny::wellPanel(
+        shiny::selectInput(
+          "region", "Region:", 
+          choices=colnames(datasets::WorldPhones)
+        ),
+        shiny::helpText("Data from AT&T (1961) The World's Telephones.")
+      )
+    ),
+    shiny::column(
+      width = 7,
+      shiny::plotOutput("phonePlot")
+    )
+  )
+)
