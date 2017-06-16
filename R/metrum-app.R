59d1cb625482b6bec1daf308a5d3c1b3cb52078f
Eric Anderson <andersone@Erics-MacBook-Pro.local>
2017-06-16 15:59:39 -0400

    updates

--- R/metrum-app.R
+++ R/metrum-app.R
@@ -1,45 +1,4 @@
-metrumApp <- function(..., logo_location = "img/metrum_new_logo.png", include_footer = TRUE){
-  
-  filesToServe <- 
-    data.frame(
-      type = c("css",
-               "js",
-               "img",
-               "img",
-               "img"),
-      name = c("metrum-app.css",
-               "metrum-app.js",
-               "favicon.ico",
-               "metrum_new_logo.png",
-               "logo_m.png")
-    )
-  
-  if(!dir.exists('www')){
-    dir.create('www')
-  }
-  
-  for(i in 1:nrow(filesToServe)){
-    
-    if(!dir.exists(file.path('www', filesToServe$type[i]))){
-      dir.create(file.path('www', filesToServe$type[i]))
-    }
-    
-    fileTypeName.i <- file.path(filesToServe$type[i], 
-                                filesToServe$name[i])
-    
-    
-    # Check if file exists and remove
-    if(file.exists(file.path("www", fileTypeName.i))){
-      file.remove(
-        file.path("www", fileTypeName.i) 
-      )
-    }
-    
-    file.copy(
-      from = system.file(fileTypeName.i, package = "shinymetrum"),
-      to = file.path("www", filesToServe$type[i])
-    )
-  }
+metrumApp <- function(..., logo_location = "https://raw.githubusercontent.com/metrumresearchgroup/shinymetrum/master/inst/img/metrum_new_logo.png", include_footer = TRUE){
   
   if(include_footer){
     
@@ -57,7 +16,7 @@ metrumApp <- function(..., logo_location = "img/metrum_new_logo.png", include_fo
             target = "_blank",
             shiny::tags$img(
               alt = "Metrum Research Group",
-              src = "img/metrum_new_logo.png"
+              src = "https://raw.githubusercontent.com/metrumresearchgroup/shinymetrum/master/inst/img/metrum_new_logo.png"
             )
           )
         )
@@ -74,21 +33,15 @@ metrumApp <- function(..., logo_location = "img/metrum_new_logo.png", include_fo
       ),
       shiny::tags$link(
         rel = "shortcut icon",
-        href = "img/favicon.ico"
+        href = "https://raw.githubusercontent.com/metrumresearchgroup/shinymetrum/master/inst/img/favicon.ico"
       ),
-      tags$link(rel="stylesheet", href = "css/metrum-app.css"),
-      tags$script(src = "js/metrum-app.js")
+      includeCSS("https://raw.githubusercontent.com/metrumresearchgroup/shinymetrum/master/inst/css/metrum-app.css"),
+      includeScript("https://raw.githubusercontent.com/metrumresearchgroup/shinymetrum/master/inst/js/metrum-app.js"),
     ),
     shiny::tags$img(
       id = "metrum-app-loading-image",
-      style = " 
-      position: absolute;
-      left: 50%;
-      top: 50%; 
-      margin-left: -210px;
-      margin-top: -100px;",
       alt = "Metrum Research Group",
-      src = "img/metrum_new_logo.png"#
+      src = "https://raw.githubusercontent.com/metrumresearchgroup/shinymetrum/master/inst/img/metrum_new_logo.png"
     ),
     shiny::tags$div(
       id = "metrum-app-content",
